b7936623324900bb908595e2781a8d7e45c90c33
Xiaofang  Niu <xiaofangniu@MacBook-Pro.local>
2021-01-30 15:12:01 +0800

    do

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,11 +1,57 @@
 <template>
-    <div>登录页面</div>
+  <div class="main">
+    <el-form ref="form" :model="form" label-width="80px">
+      <el-form-item label="用户名">
+        <el-input v-model="form.name"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="form.name"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">登录</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+
 </template>
 <script lang="ts">
 import Vue from 'vue'
 
 export default Vue.extend({
-  name: 'LoginIndex'
+  name: 'LoginIndex',
+  data () {
+      return {
+        form: {
+          name: '',
+          region: '',
+          date1: '',
+          date2: '',
+          delivery: false,
+          type: [],
+          resource: '',
+          desc: ''
+        }
+      }
+    },
+    methods: {
+      onSubmit () {
+        console.log('submit!')
+      }
+    }
 })
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.main {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .el-form {
+    padding: 20px;
+    background-color: #fff;
+    .el-button {
+      width: 100%;
+    }
+  }
+}
+</style>
